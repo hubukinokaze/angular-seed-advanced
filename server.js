5c03a7443152b6249eee3f45c87e71bf984aa842
Jun Kawa <hubukinokaze@live.com>
2017-10-02 18:53:21 -0700

    + removed local csp

--- server.js
+++ server.js
@@ -9,18 +9,12 @@ var cspPolicy = {
   "script-src" : [ CSP.SRC_SELF ]
 };
 var globalCSP = csp.getCSP(csp.STARTER_OPTIONS);
-var localCSP = csp.getCSP(cspPolicy);
 
 app.use(globalCSP);
 // Run the app by serving the static files
 // in the dist directory
 app.use(express.static(__dirname + '/dist'));
 
-app.get('/settings',
-  localCSP, // This will apply the local policy just to this page
-  function(req, res) {
-    res.render('settings');
-  });
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
