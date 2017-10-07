9c9e45b47a1d88135c88f71c7a8544b1e8a024a3
Jun Kawa <hubukinokaze@live.com>
2017-10-07 09:54:35 -0700

    + changed novel, art, and manga list to ngfor

--- src/client/app/components/home/home.component.ts
+++ src/client/app/components/home/home.component.ts
@@ -13,31 +13,128 @@ import { RouterExtensions, Config } from '../../modules/core/index';
 })
 export class HomeComponent implements OnInit {
   public names$: Observable<any>;
-  public newName: string;
+  public novelList: any;
+  public artList: any;
+  public mangaList: any;
 
   constructor(public routerext: RouterExtensions) {}
 
   ngOnInit() {
-    this.newName = '';
-  }
-
-  /*
-   * @param newname  any text as input.
-   * @returns return false to prevent default form submit behavior to refresh the page.
-   */
-  addName(): boolean {
-    this.newName = '';
-    return false;
-  }
-
-  readAbout() {
-    // Try this in the {N} app
-    // {N} can use these animation options
-    this.routerext.navigate(['/about'], {
-      transition: {
-        duration: 1000,
-        name: 'slideTop',
+    this.novelList = [
+      {
+        icon: 'collections',
+        title: 'Title',
+        author: 'Author',
+        description: 'Description',
+        date: 'Date',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'book',
+        title: 'Chapter 284: The Grand Battle between Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 4, 2017',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'book',
+        title: 'Chapter 283: The Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 3, 2017',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'book',
+        title: 'Chapter 282: Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 2, 2017',
+        cover: 'Cover Image'
       }
-    });
+    ];
+
+    this.artList = [
+      {
+        icon: 'collections',
+        title: 'Title',
+        author: 'Author',
+        description: 'Description',
+        date: 'Date',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'color_lens',
+        title: 'The Grand Battle between Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 4, 2017',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'color_lens',
+        title: 'The Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 3, 2017',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'color_lens',
+        title: 'Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 2, 2017',
+        cover: 'Cover Image'
+      }
+    ];
+
+    this.mangaList = [
+      {
+        icon: 'collections',
+        title: 'Title',
+        author: 'Author',
+        description: 'Description',
+        date: 'Date',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'dashboard',
+        title: 'Chapter 284: The Grand Battle between Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 4, 2017',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'dashboard',
+        title: 'Chapter 283: The Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 3, 2017',
+        cover: 'Cover Image'
+      },
+      {
+        icon: 'dashboard',
+        title: 'Chapter 282: Monsters',
+        author: 'Reigokai',
+        description: 'Description',
+        date: 'Oct. 2, 2017',
+        cover: 'Cover Image'
+      }
+    ];
   }
+
+
+  // readAbout() {
+  //   // Try this in the {N} app
+  //   // {N} can use these animation options
+  //   this.routerext.navigate(['/about'], {
+  //     transition: {
+  //       duration: 1000,
+  //       name: 'slideTop',
+  //     }
+  //   });
+  // }
 }
