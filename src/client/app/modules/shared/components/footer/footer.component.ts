5d62d6b8f929fdf75040e62bf98f37fbe60158d5
Jun Kawa <hubukinokaze@live.com>
2017-10-06 21:56:58 -0700

    + added footer

---
+++ src/client/app/modules/shared/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+// app
+import { Component } from '@angular/core';
+import { SharedModule } from '../../shared.module';
+
+@Component({
+  moduleId: module.id,
+  selector: 'footer',
+  templateUrl: 'footer.component.html',
+  styleUrls: [
+    'footer.component.css',
+  ],
+})
+export class FooterComponent {
+}
