5e0c6b7f183bcc93dcc953ad7786b07ec654af55
Jun Kawa <hubukinokaze@live.com>
2017-10-08 19:09:49 -0700

    + added gentium basic font + added cenzil font + changed font size

--- src/client/app/modules/shared/components/navbar/navbar.component.ts
+++ src/client/app/modules/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 // app
 import { Component } from '@angular/core';
 import { SharedModule } from '../../shared.module';
+import { trigger,style,transition,animate,keyframes,query,stagger,state } from '@angular/animations';
 
 @Component({
   moduleId: module.id,
@@ -9,6 +10,16 @@ import { SharedModule } from '../../shared.module';
   styleUrls: [
     'navbar.component.css',
   ],
+  animations: [
+    trigger('highlight', [
+      state('1, void', style({borderBottom: 'none'})),
+      state('-1', style({borderBottom: 'thick solid #3F354D'})),
+      transition(
+        'collapsed <=> expanded', [animate(500, style({height: '250px'})), animate(500)])
+    ]),
+  ]
 })
 export class NavbarComponent {
+  public navSelect: any[];
+  constructor(){this.navSelect = [1, 1, 1, 1, 1];}
 }
